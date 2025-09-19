c1651700791cc27604582fd22d5161018dd5caa4
ElijahMattera <computerclassenjoyer38@gmail.com>
2025-09-19 19:34:02 +0000

    Completed Walker Project

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -22,35 +22,31 @@ function rollDie() {
 console.log("You rolled an " + randomNum + "!!")
 alert("You've rolled a " + randomNum + " Brosynthesis :3")
 
-if (randomNum === 1) {
-  makeDot(42, 42, "#die") 
-}
-else if (randomNum === 2) {
-  makeDot(10, 10, "#die") 
-  makeDot(75, 75, "#die") 
-}
-else if (randomNum === 3) {
-  makeDot(10, 10, "#die") 
-  makeDot(75, 75, "#die") 
-  makeDot(42, 42, "#die") 
-}
-else if (randomNum === 4) {
-  makeDot(10, 10, "#die") 
-  makeDot(75, 75, "#die") 
-  makeDot(42, 42, "#die")
-  makeDot(75, 10, "#die")
-}
-else if (randomNum === 5) {
-  makeDot(10, 10, "#die") 
-  makeDot(75, 75, "#die") 
-  makeDot(42, 42, "#die")
-  makeDot(75, 10, "#die")
-  makeDot(10, 75, "#die")
-}
+  if (randomNum === 1) {
+     makeDot(50, 50, "#die"); // middle middle
+   } else if (randomNum === 2) {
+     makeDot(25, 25, "#die"); // top left
+     makeDot(75, 75, "#die"); // bottom right
+   } else if (randomNum === 3) {
+     makeDot(25, 25, "#die"); // top left
+     makeDot(75, 75, "#die"); // bottom right
+     makeDot(50, 50, "#die"); // middle middle
+   } else if (randomNum === 4) {
+     makeDot(75, 75, "#die"); // bottom right
+     makeDot(25, 25, "#die"); // top left
+     makeDot(25, 75, "#die"); // bottom left
+     makeDot(75, 25, "#die"); // top right
+   } else if (randomNum === 5) {
+     makeDot(50, 50, "#die"); // middle middle
+     makeDot(75, 75, "#die"); // bottom right
+     makeDot(25, 25, "#die"); // top left
+     makeDot(25, 75, "#die"); // bottom left
+     makeDot(75, 25, "#die"); // top right
+   }
 else {
   makeDot(10, 10, "#die") 
   makeDot(75, 75, "#die") 
-  makeDot(42, 42, "#die")
+  makeDot(42, 10, "#die")
   makeDot(75, 10, "#die")
   makeDot(10, 75, "#die")
   makeDot(42, 75, "#die")
--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,8 +1,34 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+function runProgram() {
+  $(document).on("keydown", handleKeyDown);
+  $(document).on("keyup", handleKeyUp);
+ const KEY = {
+  "LEFT": 37,
+  "UP": 38,
+  "RIGHT": 39,
+  "DOWN": 40,
+  "W": 87,
+  "A": 65,
+  "S": 83,
+  "D": 68
+};
+
+var walker = {
+  "x": 0,
+  "y": 0,
+  "speedX": 0,
+  "speedY": 0
+};
+var walker2 = {
+  "x": 100,
+  "y": 100,
+  "speedX": 0,
+  "speedY": 0
+};
+
+
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -23,20 +49,33 @@ function runProgram(){
 
   Note: You can have multiple event listeners for different types of events.
   */
-  $(document).on('eventType', handleEvent);                          
-
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
   /* 
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
-  function newFrame() {
-    
+function newFrame() {
+  repositionGameItem();
+  wallCollision();
+  redrawGameItem();
+}
 
-  }
+function repositionGameItem() {
+  walker.x = walker.speedX + walker.x;
+  walker.y = walker.speedY + walker.y;
+
+  walker2.x + walker2.speedX + walker.x;
+  walker2.y + walker2.speedY + walker.y;
+}
+
+function redrawGameItem() {
+  $("#walker").css("left", walker.x).css("top", walker.y);
+  $("#walker2").css("left", walker2.x).css("top", walker2.y);
+}
+
+
   
   /* 
   This section is where you set up the event handlers for user input.
@@ -44,10 +83,41 @@ function runProgram(){
   
   Note: You can have multiple event handlers for different types of events.
   */
-  function handleEvent(event) {
+function handleKeyDown(event) {
+  if (event.which === KEY.LEFT) walker.speedX = -5;
+  else if (event.which === KEY.RIGHT) walker.speedX = 5;
+  else if (event.which === KEY.UP) walker.speedY = -5;
+  else if (event.which === KEY.DOWN) walker.speedY = 5;
 
-  }
+  if (event.which === KEY.A) walker2.speedX = -5;
+  else if (event.which === KEY.D) walker2.speedX = 5;
+  else if (event.which === KEY.W) walker2.speedY = -5;
+  else if (event.which === KEY.S) walker2.speedY = 5;
+}
 
+function handleKeyUp(event) {
+  if (event.which === KEY.LEFT || event.which === KEY.RIGHT) walker.speedX = 0;
+  if (event.which === KEY.UP || event.which === KEY.DOWN) walker.speedY = 0;
+
+  if (event.which === KEY.A || event.which === KEY.D) walker2.speedX = 0;
+  if (event.which === KEY.W || event.which === KEY.S) walker2.speedY = 0;
+}
+
+
+function wallCollision() {
+  var boardWidth = $("#board").width();
+  var boardHeight = $("#board").height();
+
+  if (walker.x < 0) walker.x = 0;
+  if (walker.y < 0) walker.y = 0;
+  if (walker.x + $("#walker").width() > boardWidth) walker.x = boardWidth - $("#walker").width();
+  if (walker.y + $("#walker").height() > boardHeight) walker.y = boardHeight - $("#walker").height();
+
+  if (walker2.x < 0) walker2.x = 0;
+  if (walker2.y < 0) walker2.y = 0;
+  if (walker2.x + $("#walker2").width() > boardWidth) walker2.x = boardWidth - $("#walker2").width();
+  if (walker2.y + $("#walker2").height() > boardHeight) walker2.y = boardHeight - $("#walker2").height();
+}
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -60,5 +130,4 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
 }
